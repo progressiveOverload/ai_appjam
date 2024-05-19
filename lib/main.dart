beba18f317a5c3c31381bede76a78182b5fcf445
cihan <cihanisikal@outlook.com>
2024-05-19 12:10:15 +0300

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: LoginPage(),
     );
