8a40f9e1311e02535d22565276b2091ee74599d0
cihan <cihanisikal@outlook.com>
2024-05-19 10:46:45 +0300

    chore: Refactor login screen UI

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -15,34 +16,95 @@ class LoginPageState extends State<LoginPage> {
   @override
   @override
   Widget build(BuildContext context) {
+    Bool rememberMe;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
+      
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+            Image.asset(
+              'assets/icon.png',
+              width: 200,
+              height: 200,
+            ),
+            Text("Welcome"),
+            const SizedBox(height: 16.0),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.white,
+              ),
+              child: TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                ),
               ),
             ),
+          
             const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.white,
               ),
-              obscureText: true,
+              child: TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+                ),
+                obscureText: true,
+              ),
             ),
+            Row(
+              children: [
+                Radio(
+                  value: true,
+                  groupValue: (),
+                  onChanged: (value) {
+                    setState(() {
+                      // rememberMe = value;
+                    });
+                  },
+                ),
+                const Text('Remember me!'),
+              ],
+            ),
+           
             const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Login'),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Login'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Login with Google'),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text("Don't have an account? Register"),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Forgot password?'),
+                  ),
+                )
+              ],
             ),
+            
           ],
         ),
       ),
