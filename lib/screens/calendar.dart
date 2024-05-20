9ea4120234caf95a838366a3a99dd831ab73f168
cihan <cihanisikal@outlook.com>
2024-05-21 02:19:14 +0300

    added bottom nav, need to add popscope

---
+++ lib/screens/calendar.dart
@@ -0,0 +1,62 @@
+import 'package:ai_appjam/screens/favorites.dart';
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'account.dart';
+import 'chatpage.dart';
+import 'first_main_page.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      extendBody: true,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: FloatingNavbar(
+          onTap: (int val) {
+            switch (val) {
+              case 0:
+                Get.to(const FirstMainPage());
+                break;
+              case 1:
+                Get.to(const CalendarPage());
+                break;
+              case 2:
+                Get.to(const ChatScreen());
+                break;
+              case 3:
+                Get.to(const FavoritePage());
+                break;
+              case 4:
+                Get.to(const AccountPage());
+                break;
+            }
+          },
+          currentIndex: 1,
+          items: [
+            FloatingNavbarItem(
+              icon: Icons.home,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.calendar_month,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.location_on,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.favorite,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.person,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/first_main_page.dart
+++ lib/screens/first_main_page.dart
@@ -1,4 +1,9 @@
+import 'package:ai_appjam/screens/account.dart';
+import 'package:ai_appjam/screens/calendar.dart';
 import 'package:ai_appjam/screens/chatpage.dart';
+import 'package:ai_appjam/screens/favorites.dart';
+import 'package:ai_appjam/screens/map.dart';
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -307,7 +312,97 @@ class _FirstMainPageState extends State<FirstMainPage> {
         
 
       ),
-    
+      extendBody: true,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: FloatingNavbar(
+          onTap: (int val) {
+            switch (val) {
+              case 0:
+                Get.to(const FirstMainPage());
+                break;
+              case 1:
+                Get.to(const CalendarPage());
+                break;
+              case 2:
+                Get.to(const ChatScreen());
+                break;
+              case 3:
+                Get.to(const AccountPage());
+                break;
+              case 4:
+                Get.to(const AccountPage());
+                break;
+            }
+          },
+          currentIndex: 0,
+          items: [
+            FloatingNavbarItem(
+              icon: Icons.home,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.calendar_month,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.location_on,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.favorite,
+            ),
+            FloatingNavbarItem(
+              icon: Icons.person,
+            ),
+          ],
+        ),
+      ),
+      // bottomNavigationBar: ClipRRect(
+      //   borderRadius: const BorderRadius.vertical(top: Radius.circular(40)),
+      //   child: BottomNavigationBar(
+      //     items: const <BottomNavigationBarItem>[
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.home),
+      //         label: 'Home',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.calendar_today),
+      //         label: 'Calendar',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.map),
+      //         label: 'Map',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.favorite),
+      //         label: 'Favorites',
+      //       ),
+      //       BottomNavigationBarItem(
+      //         icon: Icon(Icons.account_circle),
+      //         label: 'Account',
+      //       ),
+      //     ],
+      //     onTap: (index) {
+      //       switch (index) {
+      //         case 0:
+      //           Get.to(const FirstMainPage());
+      //           break;
+      //         case 1:
+      //           Get.to(const CalendarPage());
+      //           break;
+      //         case 2:
+      //           Get.to(const MapPage());
+      //           break;
+      //         case 3:
+      //           Get.to(const FavoritePage());
+      //           break;
+      //         case 4:
+      //           Get.to(const AccountPage());
+      //           break;
+      //       }
+      //     },
+      //   ),
+      // ),
     );
+    
+    
   }
 }
