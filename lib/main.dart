7ca47f566125be13df4f0ca633e5bda1f9558259
cihan <cihanisikal@outlook.com>
2024-05-21 12:56:44 +0300

    security related, added dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
